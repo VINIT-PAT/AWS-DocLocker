b24c2f5e2a7e83fa2b5e6de83e38546f575d21fe
VINIT-PAT <144584021+VINIT-PAT@users.noreply.github.com>
2024-06-18 16:59:23 +0530

    Aws-s3 Doc Locker with Multiple Uploads

---
+++ NodejsDownloadFileAmazonS3/app/config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIAIGGOGCH2UD7H63JA', // change to yours
+	AWS_SECRET_ACCESS_KEY: 'GaGHmB7ajGsBOd6w626l6wilE+es3zqN9oT9C9Ka', // change to yours
+	REGION : 'us-east-2', // change to yours
+	Bucket: 'grokonez-s3-bucket' // change to yours
+};
+ 
+module.exports = env;
